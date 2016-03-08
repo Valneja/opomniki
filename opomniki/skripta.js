5238850c7081debc6c4161665f794a67c55badbe
Valneja <vs7315@student.uni-lj.si>
2016-03-08 07:32:55 +0000

    implementirana prijava

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -1,10 +1,16 @@
 window.addEventListener('load', function() {
 	//stran nalozena
-		
+	var izvediPrijavo = function() {
+		var uporabnik = document.querySelector("#uporabnisko_ime").value;
+		document.querySelector("#uporabnik").innerHTML = uporabnik;
+		document.querySelector(".pokrivalo").style.visibility = "hidden";
+	}	
+	document.querySelector("#prijavniGumb").addEventListener('click', izvediPrijavo);
+	
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
-		
+
 		for (i = 0; i < opomniki.length; i++) {
 			var opomnik = opomniki[i];
 			var casovnik = opomnik.querySelector("span");
