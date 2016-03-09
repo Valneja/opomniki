e074293406816bdd9b9d2f1642f72dbfb236106d
Valneja <vs7315@student.uni-lj.si>
2016-03-09 12:16:59 +0000

    Stran pripravljena za produkcijo

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -16,7 +16,7 @@ window.addEventListener('load', function() {
 		document.querySelector("#naziv_opomnika").value = "";
 		
 		document.querySelector("#opomniki").innerHTML += 
-		"<div class='opomnik'>"+
+		"<div class='opomnik rob senca'>"+
             "<div class='naziv_opomnika'>"+naziv+"</div>"+
             "<div class='cas_opomnika'> Opomnik čez <span>"+cas+"</span> sekund.</div>"+
 		"</div>"
@@ -31,7 +31,14 @@ window.addEventListener('load', function() {
 			var opomnik = opomniki[i];
 			var casovnik = opomnik.querySelector("span");
 			var cas = parseInt(casovnik.innerHTML);
-	
+			if(cas == 0){
+				alert("Opomnik!\n\nZadolžitev "+opomnik.querySelector(".naziv_opomnika").innerHTML+" je potekla!");
+				document.querySelector("#opomniki").removeChild(opomnik);
+			}
+			else{
+				cas--;
+				casovnik.innerHTML = cas;
+			}
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
